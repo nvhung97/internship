a375a813b58026b2114437eb47232a35a2a50882
nvhung97 <1511360@hcmut.edu.vn>
2018-07-13 16:41:51 +0700

    Done touchEvent

--- 009. TouchEvent/SwipeBackDemo/app/src/main/java/com/example/cpu11398_local/swipebackdemo/MyLayout.java
+++ 009. TouchEvent/SwipeBackDemo/app/src/main/java/com/example/cpu11398_local/swipebackdemo/MyLayout.java
@@ -6,55 +6,63 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.ViewConfiguration;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MyLayout extends FrameLayout{
 
-    private final float     MIN_MOVE                = 10.0f;
-    private final long      MAX_ANIMATION_DURATION  = 1000;
+    private final long      MAX_ANIMATION_DURATION      = 500;
+    private final long      MAX_FLING_SCREEN_DURATION   = 250;
+    private final long      MAX_FLING_TOM_DURATION      = 500;
 
     private float           screenWidth, screenHeight;
-    private float           tomSize;
     private float           tomMaxTranslate;
     private float           downX, downY;
-    private boolean         isMoving, isHorizontalMoving;
+
+    private float           touchSlop;
+    private float           minimumFlingVelocity, maximumFlingVelocity;
+
+    private boolean         isMoving, isHorizontalMoving, isRunningAnimation;
 
     private VelocityTracker velocityTracker;
 
+    private FrameLayout     lyt_frame;
+    private ImageView       img_background;
     private ImageView       img_tom;
-    private View            background;
 
     public MyLayout(@NonNull Context context) {
         super(context);
+        init(context);
     }
 
     public MyLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
     public MyLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        ViewConfiguration configuration = ViewConfiguration.get(context);
+        touchSlop                       = configuration.getScaledTouchSlop();
+        minimumFlingVelocity            = configuration.getScaledMinimumFlingVelocity();
+        maximumFlingVelocity            = configuration.getScaledMaximumFlingVelocity();
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        img_tom = findViewById(R.id.img_tom);
-        background = new View(getContext());
-        background.setBackgroundColor(getResources().getColor(R.color.colorLightBlack));
-        background.setVisibility(GONE);
-        addView(
-                background,
-                new ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT
-                )
-        );
+        lyt_frame       = findViewById(R.id.lyt_frame);
+        img_background  = findViewById(R.id.img_background);
+        img_tom         = findViewById(R.id.img_tom);
     }
 
     @Override
@@ -62,74 +70,103 @@ public class MyLayout extends FrameLayout{
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         screenWidth     = MeasureSpec.getSize(widthMeasureSpec);
         screenHeight    = MeasureSpec.getSize(heightMeasureSpec);
-        tomSize         = img_tom.getMeasuredWidth();
-        tomMaxTranslate = (screenHeight - tomSize) / 2;
+        tomMaxTranslate = (screenHeight - img_tom.getMeasuredWidth()) / 2;
     }
 
     @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        switch (ev.getActionMasked()) {
-            case MotionEvent.ACTION_DOWN:
-                downX = ev.getRawX();
-                downY = ev.getRawY();
-                return false;
-            case MotionEvent.ACTION_MOVE:
-                return ev.getRawX() - downX > MIN_MOVE
-                        || Math.abs(ev.getRawY() - downY) > MIN_MOVE;
-            default:
-                return false;
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (isRunningAnimation) {
+            return true;
         }
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                considerActionDown(event);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                considerActionMove(event);
+                break;
+        }
+        return isMoving;
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getActionMasked()) {
-            case MotionEvent.ACTION_MOVE:
-                float deltaX = event.getRawX() - downX;
-                float deltaY = event.getRawY() - downY;
-                if (!isMoving) {
-                    isMoving = deltaX > MIN_MOVE || Math.abs(deltaY) > MIN_MOVE;
-                    isHorizontalMoving = deltaX > Math.abs(deltaY);
-                }
-                if (isMoving) {
-                    if (isHorizontalMoving) {
-                        translateScreenManual(deltaX);
-                    } else {
-                        translateTomManual(deltaY);
-                    }
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-                if (isMoving) {
-                    if (isHorizontalMoving) {
-                        translateScreenAutomatic(event.getRawX() - downX);
-                    } else {
-                        translateTomAutomatic(event.getRawY() - downY);
-                    }
-                    isMoving = false;
-                }
-                break;
+        if (!isRunningAnimation) {
+            switch (event.getActionMasked()) {
+                case MotionEvent.ACTION_MOVE:
+                    considerActionMove(event);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    considerActionUp(event);
+                    break;
+            }
         }
         return true;
     }
 
+    private void considerActionDown(MotionEvent event) {
+        downX = event.getRawX();
+        downY = event.getRawY();
+    }
+
+    private void considerActionMove(MotionEvent event) {
+        float deltaX = event.getRawX() - downX;
+        float deltaY = event.getRawY() - downY;
+        if (!isMoving) {
+            if (deltaX > touchSlop || Math.abs(deltaY) > touchSlop) {
+                isMoving = true;
+                isHorizontalMoving = deltaX > Math.abs(deltaY);
+                velocityTracker = VelocityTracker.obtain();
+            }
+        }
+        if (isMoving) {
+            if (isHorizontalMoving) {
+                translateScreenManual(deltaX);
+            } else {
+                translateTomManual(deltaY);
+            }
+            velocityTracker.addMovement(event);
+        }
+    }
+
+    private void considerActionUp(MotionEvent event) {
+        if (isMoving) {
+            velocityTracker.addMovement(event);
+            velocityTracker.computeCurrentVelocity(1000, maximumFlingVelocity);
+            if (isHorizontalMoving) {
+                if (Math.abs(velocityTracker.getXVelocity()) > minimumFlingVelocity) {
+                    flingScreen(velocityTracker.getXVelocity(), event.getRawX() - downX);
+                } else {
+                    translateScreenAutomatic(event.getRawX() - downX);
+                }
+            } else {
+                if (Math.abs(velocityTracker.getYVelocity()) > minimumFlingVelocity) {
+                    flingTom(velocityTracker.getYVelocity(), event.getRawY() - downY);
+                } else {
+                    translateTomAutomatic(event.getRawY() - downY);
+                }
+            }
+            isMoving = false;
+            velocityTracker.clear();
+        }
+    }
+
     private void translateScreenManual(float translate) {
+        setScreenAlphaWithTranslate(translate);
         if (translate < 0) {
-            img_tom.setImageResource(R.drawable.img_tom_wait_touch);
+            setTomWaitTouch();
             downX += translate;
-            this.setTranslationX(0);
+            lyt_frame.setTranslationX(0);
         } else {
-            img_tom.setImageResource(R.drawable.img_tom_afraid);
-            this.setTranslationX(translate);
+            setTomFraid();
+            lyt_frame.setTranslationX(translate);
         }
     }
 
     private void translateTomManual(float translate) {
         if (Math.abs(translate) <= tomMaxTranslate) {
-            img_tom.setImageResource(R.drawable.img_tom_wait_touch);
             img_tom.setTranslationY(translate);
         } else {
-            img_tom.setImageResource(R.drawable.img_tom_laugh);
             if (translate > 0) {
                 img_tom.setTranslationY(tomMaxTranslate);
                 downY += translate - tomMaxTranslate;
@@ -142,19 +179,22 @@ public class MyLayout extends FrameLayout{
 
     private void translateScreenAutomatic(float translate) {
         if (translate < screenWidth / 2) {
-            this
+            long duration = (long)(translate / screenWidth * MAX_ANIMATION_DURATION);
+            img_background.animate().alpha(1.0f).setDuration(duration);
+            lyt_frame
                     .animate()
                     .translationX(0)
-                    .setDuration((long)(translate / screenWidth * MAX_ANIMATION_DURATION))
+                    .setDuration(duration)
                     .setListener(new Animator.AnimatorListener() {
                         @Override
                         public void onAnimationStart(Animator animation) {
-
+                            isRunningAnimation = true;
                         }
 
                         @Override
                         public void onAnimationEnd(Animator animation) {
-                            img_tom.setImageResource(R.drawable.img_tom_wait_touch);
+                            setTomWaitTouch();
+                            isRunningAnimation = false;
                         }
 
                         @Override
@@ -169,14 +209,15 @@ public class MyLayout extends FrameLayout{
                     });
         }
         else {
-            this
+            long duration = (long)((screenWidth - translate) / screenWidth * MAX_ANIMATION_DURATION);
+            img_background.animate().alpha(0.0f).setDuration(duration);
+            lyt_frame
                     .animate()
                     .translationX(screenWidth)
-                    .setDuration((long)((screenWidth - translate) / screenWidth * MAX_ANIMATION_DURATION))
+                    .setDuration(duration)
                     .setListener(new Animator.AnimatorListener() {
                         @Override
                         public void onAnimationStart(Animator animation) {
-
                         }
 
                         @Override
@@ -199,10 +240,215 @@ public class MyLayout extends FrameLayout{
     }
 
     private void translateTomAutomatic(float translate) {
-        img_tom.setImageResource(R.drawable.img_tom_wait_touch);
+        setTomWaitTouch();
         img_tom
                 .animate()
                 .translationY(0)
-                .setDuration((long)(Math.abs(translate) / tomMaxTranslate * MAX_ANIMATION_DURATION));
+                .setDuration((long)(Math.abs(translate) / tomMaxTranslate / 2 * MAX_ANIMATION_DURATION))
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        isRunningAnimation = true;
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        isRunningAnimation = false;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+    }
+
+    private void flingScreen(float velocity, float translate) {
+        if (velocity > 0) {
+            long duration = (long)((screenWidth - translate) / screenWidth * MAX_FLING_SCREEN_DURATION);
+            img_background.animate().alpha(0.0f).setDuration(duration);
+            lyt_frame
+                    .animate()
+                    .translationX(screenWidth)
+                    .setDuration(duration)
+                    .setListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            ((Activity)getContext()).finish();
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+
+                        }
+                    });
+        } else {
+            long duration = (long)(translate / screenWidth * MAX_FLING_SCREEN_DURATION);
+            img_background.animate().alpha(1.0f).setDuration(duration);
+            lyt_frame
+                    .animate()
+                    .translationX(0)
+                    .setDuration(duration)
+                    .setListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                            isRunningAnimation = true;
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            setTomWaitTouch();
+                            isRunningAnimation = false;
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+
+                        }
+                    });
+        }
+    }
+
+    private void flingTom(float velocity, float translate) {
+        setTomLaugh();
+        if (velocity > 0) {
+            img_tom
+                    .animate()
+                    .translationY(screenHeight / 2)
+                    .setDuration((long)((screenHeight / 2 - translate) / screenHeight * MAX_FLING_TOM_DURATION))
+                    .setListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                            isRunningAnimation = true;
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            img_tom
+                                    .animate()
+                                    .translationY(0)
+                                    .setDuration(MAX_FLING_TOM_DURATION / 2)
+                                    .setListener(new Animator.AnimatorListener() {
+                                        @Override
+                                        public void onAnimationStart(Animator animation) {
+
+                                        }
+
+                                        @Override
+                                        public void onAnimationEnd(Animator animation) {
+                                            isRunningAnimation = false;
+                                            setTomWaitTouch();
+                                        }
+
+                                        @Override
+                                        public void onAnimationCancel(Animator animation) {
+
+                                        }
+
+                                        @Override
+                                        public void onAnimationRepeat(Animator animation) {
+
+                                        }
+                                    });
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+
+                        }
+                    });
+        }
+        else {
+            img_tom
+                    .animate()
+                    .translationY(-screenHeight / 2)
+                    .setDuration((long)((screenHeight / 2 + translate) / screenHeight * MAX_FLING_TOM_DURATION))
+                    .setListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                            isRunningAnimation = true;
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            img_tom
+                                    .animate()
+                                    .translationY(0)
+                                    .setDuration(MAX_FLING_TOM_DURATION / 2)
+                                    .setListener(new Animator.AnimatorListener() {
+                                        @Override
+                                        public void onAnimationStart(Animator animation) {
+
+                                        }
+
+                                        @Override
+                                        public void onAnimationEnd(Animator animation) {
+                                            isRunningAnimation = false;
+                                            setTomWaitTouch();
+                                        }
+
+                                        @Override
+                                        public void onAnimationCancel(Animator animation) {
+
+                                        }
+
+                                        @Override
+                                        public void onAnimationRepeat(Animator animation) {
+
+                                        }
+                                    });
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+
+                        }
+                    });
+        }
+    }
+
+    private void setTomWaitTouch() {
+        img_tom.setImageResource(R.drawable.img_tom_wait_touch);
+    }
+
+    private void setTomLaugh() {
+        img_tom.setImageResource(R.drawable.img_tom_laugh);
+    }
+
+    private void setTomFraid() {
+        img_tom.setImageResource(R.drawable.img_tom_afraid);
+    }
+
+    private void setScreenAlphaWithTranslate(float translate) {
+        img_background.setAlpha((screenWidth - translate) / screenWidth);
     }
 }
--- 009. TouchEvent/SwipeBackDemo/app/src/main/java/com/example/cpu11398_local/swipebackdemo/TomActivity.java
+++ 009. TouchEvent/SwipeBackDemo/app/src/main/java/com/example/cpu11398_local/swipebackdemo/TomActivity.java
@@ -1,43 +1,19 @@
 package com.example.cpu11398_local.swipebackdemo;
 
-import android.annotation.SuppressLint;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.widget.ImageView;
 
 public class TomActivity extends AppCompatActivity {
 
-    ImageView img_tom;
-
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tom);
 
-        img_tom = findViewById(R.id.img_tom);
-
-        img_tom.setOnTouchListener((v, event) -> {
-            switch (event.getActionMasked()) {
-                case MotionEvent.ACTION_DOWN:
-                    img_tom.setImageResource(R.drawable.img_tom_tickled);
-                    break;
-                case MotionEvent.ACTION_UP:
-                case MotionEvent.ACTION_CANCEL:
-                    img_tom.setImageResource(R.drawable.img_tom_wait_touch);
-                    break;
-            }
-            return true;
-        });
-
-        greetingTom();
-    }
-
-    void greetingTom() {
-        img_tom.setBackground(getDrawable(R.drawable.img_tom_greeting));
-        AnimationDrawable animation = (AnimationDrawable)img_tom.getBackground();
+        MyView img_tom              = findViewById(R.id.img_tom);
+        AnimationDrawable animation = (AnimationDrawable)getDrawable(R.drawable.img_tom_greeting);
+        img_tom.setImageDrawable(animation);
         animation.start();
     }
 }
